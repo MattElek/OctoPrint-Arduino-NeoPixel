047d7a0dd67c9ac4525f0c1f0e67b75e1029d95e
Matt <mhar9000@gmail.com>
2018-01-22 18:26:30 -0500

    Added missing quotations

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/mhar9000/OctoPrint-Arduino-NeoPixel"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = [pyserial >= 2.7]
+plugin_requires = ["pyserial >= 2.7"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
