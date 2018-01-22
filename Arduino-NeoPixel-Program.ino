7667aa739713d4fb207fd156abba23a824dc62dc
Matthew <mhar9000@gmail.com>
2018-01-22 17:03:32 -0500

    First commit

---
+++ Arduino-NeoPixel-Program.ino
@@ -0,0 +1,102 @@
+#include <Adafruit_NeoPixel.h>
+
+// Gamma correction
+extern const uint8_t gamma8[];
+
+const byte numChars = 32;
+char receivedChars[numChars];
+boolean newData = false;
+
+// SET NEOPIXEL STRIP VARIABLES
+#define PIN 6 // What pin are the NeoPixels connected to?
+#define LEDs 30 // How many LEDs are on your NeoPixel strip?
+
+// Define NeoPixel strip
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDs, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  Serial.begin(9600);
+  strip.begin(); // Initilize NeoPixel strip
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() {
+  recvWithStartEndMarkers();
+  showColors();
+}
+
+void recvWithStartEndMarkers() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
+
+  while (Serial.available() > 0 && newData == false) {
+    rc = Serial.read();
+
+    if (recvInProgress == true) {
+      if (rc != endMarker) {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
+      }
+    }
+
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
+}
+
+void showColors() {
+  if (newData == true) {
+    // Get rid of '#' and convert it to integer
+    long number = strtol( &receivedChars[1], NULL, 16);
+
+    // Split them up into r, g, b values
+    long red = number >> 16;
+    long green = number >> 8 & 0xFF;
+    long blue = number & 0xFF;
+
+    colorWipe(strip.Color(pgm_read_byte(&gamma8[red]), pgm_read_byte(&gamma8[green]), pgm_read_byte(&gamma8[blue])), 50);
+    newData = false;
+  }
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Gamma correction
+const uint8_t PROGMEM gamma8[] = {
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+  2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+  5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+  10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+  17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+  25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+  37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+  51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+  69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+  90, 92, 93, 95, 96, 98, 99, 101, 102, 104, 105, 107, 109, 110, 112, 114,
+  115, 117, 119, 120, 122, 124, 126, 127, 129, 131, 133, 135, 137, 138, 140, 142,
+  144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 167, 169, 171, 173, 175,
+  177, 180, 182, 184, 186, 189, 191, 193, 196, 198, 200, 203, 205, 208, 210, 213,
+  215, 218, 220, 223, 225, 228, 231, 233, 236, 239, 241, 244, 247, 249, 252, 255
+};
---
+++ octoprint_arduino_neopixel/__init__.py
@@ -0,0 +1,112 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+from serial import Serial, SerialException
+
+class Arduino_neopixelPlugin(octoprint.plugin.StartupPlugin,
+                             octoprint.plugin.ShutdownPlugin,
+                             octoprint.plugin.SettingsPlugin,
+                             octoprint.plugin.TemplatePlugin,
+                             octoprint.plugin.EventHandlerPlugin,
+                             #octoprint.plugin.AssetPlugin,
+                             octoprint.plugin.BlueprintPlugin):
+
+        def __init__(self):
+            self.is_connected = False
+
+	##~~ StartupPlugin
+	def on_after_startup(self):
+		port = self._settings.get(["Port"])
+		baudrate = self._settings.get(["BaudRate"])
+		try:
+			self.board = Serial(port, baudrate, timeout=2)
+			if self.board.isOpen():
+				self._logger.info("Connected to Arduino")
+				self.is_connected = True
+			else:
+				 raise SerialException
+		except SerialException:
+			self._logger.exception("Could not connect to Arduino")
+
+	##~~ ShutdownPlugin
+	def on_shutdown(self):
+		if self.is_connected == True:
+			self.board.write("<#000000>")
+			self.board.close()
+			self._logger.info("Disconnected from Arduino")
+
+	##~~ SettingsPlugin
+	def get_settings_defaults(self):
+		return dict(
+			Connected="#000000",
+			Disconnected="#000000",
+			PrintStarted="#FFFFFF",
+			PrintFailed="#FF0000",
+			PrintDone="#0000FF",
+			PrintCancelled="#FFA500",
+			PrintPaused="#FFFF00",
+			PrintResumed="#FFFFFF",
+			Home="#00FF00",
+			Port="/dev/ttyACM1",
+			BaudRate="9600",
+		)
+
+	def get_template_configs(self):
+		return [
+			dict(type="settings", custom_bindings=False),
+			dict(type="tab", custom_bindings=False),
+		]
+
+	##~~ AssetPlugin
+	#def get_assets(self):
+	#	return dict(
+	#		js=["js/jscolor.js"],
+	#	)
+
+	##~~ EventHandlerPlugin
+	def on_event(self, event, payload):
+		if self.is_connected == True:
+                    command = self._settings.get([event])
+		    if command is not None:
+                        self.board.write("<" + command  + ">")
+
+	##~~ Softwareupdate hook
+	def get_update_information(self):
+		return dict(
+			arduino_neopixel=dict(
+				displayName="Arduino_neopixel Plugin",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="mhar9000",
+				repo="OctoPrint-Arduino-NeoPixel",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/mhar9000/OctoPrint-Arduino-NeoPixel/archive/{target_version}.zip"
+			)
+		)
+	##~~ BlueprintPlugin
+	@octoprint.plugin.BlueprintPlugin.route("/<color>/", methods=["GET"])
+	def red(self, color):
+		self.board.write("<#" + str(color) + ">")
+		return("")
+
+	def is_blueprint_protected(self):
+		return False
+
+
+##~~ Load Plugin
+__plugin_name__ = "NeoPixel"
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = Arduino_neopixelPlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
+
